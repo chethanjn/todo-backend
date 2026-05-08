1ee5d2175a9440dab6df0ad203bf6c998cfbad77
chethan <chethan.jn@oracle.com>
2026-05-08 09:55:37 +0530

    cors

--- src/main/java/com/todoapp/todo/security/SecurityConfig.java
+++ src/main/java/com/todoapp/todo/security/SecurityConfig.java
@@ -66,7 +66,8 @@ public class SecurityConfig {
     @Bean
     public CorsConfigurationSource corsConfigurationSource() {
         CorsConfiguration configuration = new CorsConfiguration();
-        configuration.setAllowedOrigins(List.of("http://localhost:4200"));
+        configuration.setAllowedOrigins(List.of("http://localhost:4200","https://todochethuapp.netlify.app"));
+
         configuration.setAllowedMethods(List.of("GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS"));
         configuration.setAllowedHeaders(List.of("*"));
         configuration.setAllowCredentials(true);
